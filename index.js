d61450bf117bf06e5392c01a701fc3e2d955de84
peterjwms <peterjwms@gmail.com>
2024-04-10 00:49:40 -0600

    initial framework and functionality for index

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app.use(express.json());
+// TODO: may need to do something else here for parsing the xml??
+
+app.use(express.static('public'));
+
+var apiRouter = express.Router();
+app.use('/api', apiRouter);
+
+// getGames
+
+// getScores
+
+// addGame
+
+// addScore
+
+// search?? this is the third-party API
+// probably just goes into search.js - use simon-service/about.js as template
+
+app.use((_req, res) => {
+    res.sendFile('index.html', {root: 'public'});
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});

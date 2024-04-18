33dea5c1f368d2d5292c5db68051951162d48287
peterjwms <peterjwms@gmail.com>
2024-04-17 22:39:32 -0600

    app routing

--- src/app.jsx
+++ src/app.jsx
@@ -1,49 +1,71 @@
 import React from 'react';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Profile } from './profile/profile';
+import { Score } from './score/score';
+import { Notifications } from './notifications/notifications';
+import { Search } from './search/search';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-    return (
-        <div className='body bg-dark text-light'>
-            <header className="container-fluid">
-                <nav className="navbar fixed-top navbar-dark">
-                    <div className="navbar-brand">OnBoard</div>
-                    <menu className="navbar-nav">
-                        <li className='nav-item'>
-                            <a href="index.html" className="nav-link">
-                                Home
-                            </a>
-                        </li>
-                        <li className='nav-item'>
-                            <a href="profile.html" className="nav-link">
-                                Profile
-                            </a>
-                        </li>
-                        <li className='nav-item'>
-                            <a href="notifications.html" className="nav-link">
-                                Activity
-                            </a>
-                        </li>
-                        <li className='nav-item'>
-                            <a href="search.html" className="nav-link">
-                                Search
-                            </a>
-                        </li>
-                    </menu>
-                </nav>
-            </header>
+  // const root = ReactDOM.createRoot(document.getElementById('root'));
+  return (
+    <BrowserRouter>
+      <div className='body bg-dark text-light'>
+        <header className="container-fluid">
+          <nav className="navbar fixed-top navbar-dark">
+            <div className="navbar-brand">OnBoard</div>
+            <menu className="navbar-nav">
+              <li className='nav-item'>
+                <NavLink to="" className="nav-link">
+                  Login
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink to="profile" className="nav-link">
+                  Profile
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink to="score" className="nav-link">
+                  Score
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink to="notifications" className="nav-link">
+                  Activity
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink to="search" className="nav-link">
+                  Search
+                </NavLink>
+              </li>
+            </menu>
+          </nav>
+        </header>
 
-            <main className='container-fluid'>App components will display here</main>
+        <Routes>
+          <Route path="/" element={<Login />} exact />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/score" element={<Score />} />
+          <Route path="/notifications" element={<Notifications />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="*" element={<NotFound/>} />
+        </Routes>
 
-            <footer className="container-fluid">
-                {/* <div className='container-fluid'> */}
-                    <span className='text-reset'>Peter Williams</span>
-                    <a className='text-reset' href="https://github.com/peterjwms/startup" target="_blank">
-                        GitHub
-                    </a>
-                {/* </div> */}
-            </footer>
-        </div>
-    );
+        <footer className="container-fluid">
+          <span className='text-reset'>Peter Williams</span>
+          <a className='text-reset' href="https://github.com/peterjwms/startup" target="_blank">
+            GitHub
+          </a>
+        </footer>
+      </div>
+    </BrowserRouter>
+  );
 }
 
+function NotFound() {
+  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
+}
